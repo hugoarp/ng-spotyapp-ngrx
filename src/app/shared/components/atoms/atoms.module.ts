f267c76f01633c35b41dca1056e1e85dff77eeca
Hugo Arias <hugo.arias@pragma.com.co>
2022-08-29 13:52:26 -0500

    ➕ ADD: Se crean los módulos para atoms, molecules y organisms

---
+++ src/app/shared/components/atoms/atoms.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GreenButtonComponent } from './green-button/green-button.component';
+
+@NgModule({
+  declarations: [GreenButtonComponent],
+  imports: [CommonModule],
+  exports: [GreenButtonComponent],
+})
+export class AtomsModule {}
