fced74882fd31c6d5dba63cf55b4eb9d21b7f536
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:53:13 -0500

    ➕ ADD: Se crea el módulo de organismos

--- src/app/shared/components/organisms/organisms.module.ts
+++ src/app/shared/components/organisms/organisms.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MoleculesModule } from '../molecules/molecules.module';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { TrackCardComponent } from './track-card/track-card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [NavMenuComponent, TrackCardComponent],
+  imports: [CommonModule, MoleculesModule, RouterModule],
+  exports: [NavMenuComponent, TrackCardComponent],
 })
-export class OrganismsModule { }
+export class OrganismsModule {}
