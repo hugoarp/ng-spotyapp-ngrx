c1d52c8b92218dd657f0856387d24dc04514ab14
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:53:00 -0500

    ➕ ADD: Se crea la molécula user-button

---
+++ src/app/shared/components/molecules/user-button/user-button.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { UserResponse } from 'src/app/core/services/interfaces/user';
+
+@Component({
+  selector: 'app-user-button',
+  templateUrl: './user-button.component.html',
+  styleUrls: ['./user-button.component.scss'],
+})
+export class UserButtonComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
