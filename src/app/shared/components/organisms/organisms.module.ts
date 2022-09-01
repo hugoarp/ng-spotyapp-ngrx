b06498bba4e513fe9a236f01bd8928f4545365db
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 15:07:20 -0500

    💀 DELETE: Se elimina el oganismo track-card

--- src/app/shared/components/organisms/organisms.module.ts
+++ src/app/shared/components/organisms/organisms.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MoleculesModule } from '../molecules/molecules.module';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { TrackCardComponent } from './track-card/track-card.component';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [NavMenuComponent, TrackCardComponent],
+  declarations: [NavMenuComponent],
   imports: [CommonModule, MoleculesModule, RouterModule],
-  exports: [NavMenuComponent, TrackCardComponent],
+  exports: [NavMenuComponent],
 })
 export class OrganismsModule {}
