2fdc8cd4dcd3560dd2020a91820b46ef8085cd5a
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:41:57 -0500

    ➕ ADD: Se crea servicio para las consultas a la API de spotify

--- src/app/core/services/spotify.service.ts
+++ src/app/core/services/spotify.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { RecommendationsResponse } from './interfaces/recommendations';
+import { FavoritesResponse } from './interfaces/favorites';
+import { UserResponse } from './interfaces/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
-  constructor() {}
+  accessToken: string = '';
 
-  getAccessToken() {}
+  constructor(private http: HttpClient) {}
+
+  getCurrentUser() {
+    return this.http.get<UserResponse>(`${environment.url}/me`);
+  }
+
+  getRecommendations() {
+    return this.http.get<RecommendationsResponse>(
+      `${environment.url}/recommendations?limit=10&market=ES&seed_genres=rock`
+    );
+  }
+
+  getUserFavorites() {
+    return this.http.get<FavoritesResponse>(`${environment.url}/me/tracks`);
+  }
 }
