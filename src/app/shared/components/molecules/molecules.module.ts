0ef4933db1d6d9ef31a03554f36c6d7d0438ebcd
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:52:01 -0500

    ➕ ADD: Se crea el módulo de molecules

--- src/app/shared/components/molecules/molecules.module.ts
+++ src/app/shared/components/molecules/molecules.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NavButtonComponent } from './nav-button/nav-button.component';
+import { AtomsModule } from '../atoms/atoms.module';
+import { TrackDetailsComponent } from './track-details/track-details.component';
+import { UserButtonComponent } from './user-button/user-button.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    NavButtonComponent,
+    TrackDetailsComponent,
+    UserButtonComponent,
+  ],
+  imports: [CommonModule, AtomsModule],
+  exports: [NavButtonComponent, TrackDetailsComponent, UserButtonComponent],
 })
-export class MoleculesModule { }
+export class MoleculesModule {}
