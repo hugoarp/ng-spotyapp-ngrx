e40d52cc28e02826828312d57f180af6877ab659
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 15:02:13 -0500

    ♻ REFACTOR: Se mapean las respuestas de algunas consultas para limpiar la data

--- src/app/core/services/spotify.service.ts
+++ src/app/core/services/spotify.service.ts
@@ -4,26 +4,50 @@ import { environment } from 'src/environments/environment';
 import { RecommendationsResponse } from './interfaces/recommendations';
 import { FavoritesResponse } from './interfaces/favorites';
 import { UserResponse } from './interfaces/user';
+import { map, Observable } from 'rxjs';
+import { TrackInfo } from './interfaces/track';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
-  accessToken: string = '';
-
   constructor(private http: HttpClient) {}
 
   getCurrentUser() {
     return this.http.get<UserResponse>(`${environment.url}/me`);
   }
 
-  getRecommendations() {
-    return this.http.get<RecommendationsResponse>(
-      `${environment.url}/recommendations?limit=10&market=ES&seed_genres=rock`
-    );
+  getRecommendations(): Observable<TrackInfo[]> {
+    return this.http
+      .get<RecommendationsResponse>(
+        `${environment.url}/recommendations?limit=10&market=ES&seed_genres=rock`
+      )
+      .pipe(
+        map(({ tracks }) =>
+          tracks.map((track) => ({
+            cover: track.album.images[0].url,
+            title: track.name,
+            description: track.artists[0].name,
+            id: track.id,
+          }))
+        )
+      );
   }
 
-  getUserFavorites() {
-    return this.http.get<FavoritesResponse>(`${environment.url}/me/tracks`);
+  getUserFavorites(): Observable<TrackInfo[]> {
+    return this.http
+      .get<FavoritesResponse>(`${environment.url}/me/tracks`)
+      .pipe(
+        map(({ items }) =>
+          items
+            .map((item) => item.track)
+            .map((track) => ({
+              cover: track.album.images[0].url,
+              title: track.name,
+              description: track.artists[0].name,
+              id: track.id,
+            }))
+        )
+      );
   }
 }
