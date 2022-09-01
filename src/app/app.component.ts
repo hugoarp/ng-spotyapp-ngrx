3822bc82bfa07ef49f09fc3bc3ae101a08d8acc3
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:26:19 -0500

    ➕ ADD: Se agrega nav-menu en app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'my-music-app-challenge';
+  menuItems: any[] = [];
+
+  constructor() {
+    this.menuItems = [
+      {
+        icon: 'home',
+        label: 'Inicio',
+        route: '/',
+      },
+      {
+        icon: 'favorite',
+        label: 'Favoritos',
+        route: '/favoritos',
+      },
+    ];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { AuthInterceptorService } from './core/interceptors/auth-interceptor.service';
+import { OrganismsModule } from './shared/components/organisms/organisms.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CoreModule,
+    OrganismsModule,
+    HttpClientModule,
+    RouterModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
