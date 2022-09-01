d751410cb9c5e9070a67b778fb896e485d9558e3
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:22:53 -0500

    ➕ ADD: Se agrega url y authParams en environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,11 @@
 
 export const environment = {
   production: false,
-
+  url: 'https://api.spotify.com/v1',
   clientID: 'c8fe2981a91043f2bc812f59ffd2582b',
   clientSecret: '3b069cc03c9d49298984d8bfd957b3b6',
+  authParams:
+    '&response_type=token&redirect_uri=http://localhost:4200/login&scope=user-library-modify user-library-read',
 };
 
 /*
