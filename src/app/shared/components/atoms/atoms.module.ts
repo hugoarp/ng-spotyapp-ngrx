ee9fa76571109115139b02e70d4a6b8bbb5289d3
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-01 10:50:29 -0500

    ➕ ADD: Se crea el módulo de atoms

--- src/app/shared/components/atoms/atoms.module.ts
+++ src/app/shared/components/atoms/atoms.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GreenButtonComponent } from './green-button/green-button.component';
+import { TextAComponent } from './text-a/text-a.component';
+import { GIconComponent } from './g-icon/g-icon.component';
+import { CoverImgComponent } from './cover-img/cover-img.component';
+import { TextBComponent } from './text-b/text-b.component';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
-  declarations: [GreenButtonComponent],
+  declarations: [
+    GreenButtonComponent,
+    TextAComponent,
+    GIconComponent,
+    CoverImgComponent,
+    TextBComponent,
+    AvatarComponent,
+  ],
   imports: [CommonModule],
-  exports: [GreenButtonComponent],
+  exports: [
+    GreenButtonComponent,
+    TextAComponent,
+    GIconComponent,
+    CoverImgComponent,
+    TextBComponent,
+    AvatarComponent
+  ],
 })
 export class AtomsModule {}
