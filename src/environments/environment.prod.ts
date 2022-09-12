a0854258248f3dbe68386ac39c47f9ae4567f008
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-12 14:22:28 -0500

    ♻ REFACTOR: Se cambia redirect_uri en archivo environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,5 @@ export const environment = {
   clientID: 'c8fe2981a91043f2bc812f59ffd2582b',
   clientSecret: '3b069cc03c9d49298984d8bfd957b3b6',
   authParams:
-    '&response_type=token&redirect_uri=https://spotyapp-ngrx.netlify.app/login/login&scope=user-library-modify user-library-read',
+    '&response_type=token&redirect_uri=https://spotyapp-ngrx.netlify.app/login&scope=user-library-modify user-library-read',
 };
