164ea3a22c441551657eb0f1bef84813d8c72406
Hugo Arias <hugo.arias@pragma.com.co>
2022-09-12 13:06:35 -0500

    ♻ REFACTOR: Se modifica archivo environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.spotify.com/v1',
+  clientID: 'c8fe2981a91043f2bc812f59ffd2582b',
+  clientSecret: '3b069cc03c9d49298984d8bfd957b3b6',
+  authParams:
+    '&response_type=token&redirect_uri=http://localhost:4200/login&scope=user-library-modify user-library-read',
 };
